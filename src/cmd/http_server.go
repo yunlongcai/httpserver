dc7024daca2185f626fe6e90032c0765e6b0feda
caiyunlong <caiyunlong2010@gmail.com>
2017-09-24 16:37:02 +0800

    bind 0.0.0.0

--- src/cmd/http_server.go
+++ src/cmd/http_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "net/http"
 )
 
@@ -10,6 +11,10 @@ func sayHello(w http.ResponseWriter, req *http.Request) {
 
 func main() {
     http.HandleFunc("/hello", sayHello)
-    http.ListenAndServe(":8001", nil);
+    err := http.ListenAndServe("0.0.0.0:8001", nil);
+    if err != nil {
+	fmt.Printf("error %s\n", err.Error())
+	return
+    }
     select{}
 }
